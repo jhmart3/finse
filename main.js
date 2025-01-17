724a1e08f1f82426daa2a020815d220017b75c48
Jack Martin <jmartin2017@gmail.com>
2025-01-16 20:53:14 -0600

    Add react

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const windowStateKeeper = require('electron-window-state');
+const sqlite3 = require('sqlite3').verbose();
 
 const isDev = process.env.NODE_ENV === 'development';
 const isMac = process.platform === 'darwin';
 let mainWindow;
+
+let db = new sqlite3.Database(isDev ? path.join(__dirname, './user_data/db.sqlite') : app.getPath('userData'), (err) => {
+    if (err) {
+        console.error('Failed to connect to database', err);
+    } else {
+        console.log('Connected to database successfully');
+        db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, dob TEXT)');
+    }
+});
 
 // Build Main Window
 function createMainWindow() {
@@ -17,7 +27,11 @@ function createMainWindow() {
         x: mainWindowState.x,
         y: mainWindowState.y,
         width: mainWindowState.width,
-        height: mainWindowState.height
+        height: mainWindowState.height,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
     })
     if (isDev) {
         mainWindow.webContents.openDevTools();
@@ -25,6 +39,18 @@ function createMainWindow() {
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
     mainWindowState.manage(mainWindow);
 }
+
+//Add data to SQLite3 database
+ipcMain.on('add-user', (event, data) => {
+    const { name, dob } = data;
+    db.run('INSERT INTO users (name, dob) VALUES (?, ?)', [name, dob], function(err) {
+        if (err) {
+            console.error('Error inserting user:', err);
+        } else {
+            console.log('User added successfully');
+        }
+    });
+});
 
 // Launch App When Ready
 app.whenReady().then(() => {
